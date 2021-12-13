60ae330da8720ddbebe67dc3f0045d1c0448dcdf
new-wang <1569566735@qq.com>
2021-12-13 16:58:22 +0800

    +：teleport演示

--- demo/vite-vue/src/components/HelloWorld.vue
+++ demo/vite-vue/src/components/HelloWorld.vue
@@ -2,13 +2,22 @@
   <h1>{{ msg }}</h1>
   <button @click="count++">count is: {{ count }}</button>
   <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+
+   <ModelButton></ModelButton>  
+
+
 </template>
 
 <script>
+import ModelButton from "./ModelButton.vue"
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  components:{
+    ModelButton
   },
   data() {
     return {
