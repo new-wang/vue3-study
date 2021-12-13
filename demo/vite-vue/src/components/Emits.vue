43d06cb31e0d6f6e697f691b4810b0fd9c1a5156
new-wang <1569566735@qq.com>
2021-12-13 17:23:26 +0800

    +：emits演示

---
+++ demo/vite-vue/src/components/Emits.vue
@@ -0,0 +1,12 @@
+<template>
+  <div @click="$emit('my-click')">
+    <h3>自定义事件</h3>
+  </div>
+</template>
+
+<script>
+export default {
+//   emits: ['click']
+  emits: ['my-click']
+}
+</script>
--- demo/vite-vue/src/components/ModelButton.vue
+++ demo/vite-vue/src/components/ModelButton.vue
@@ -17,7 +17,7 @@
 export default {
   data() {
     return {
-      modalOpen: true
+      modalOpen: false
     }
   },
 };
