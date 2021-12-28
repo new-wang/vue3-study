5553b2eebd82dfb7325f06e628ebf3de031cc697
new-wang <1569566735@qq.com>
2021-12-28 18:29:14 +0800

    +：抽离todo和filter相关逻辑；

--- demo/vite-vue/src/components/todos/Todos.vue
+++ demo/vite-vue/src/components/todos/Todos.vue
@@ -15,110 +15,54 @@
       placeholder="新增今日待办"
       autocomplete="off"
     ></EditTodo>
-
+    
     <!-- todo 列表 -->
     <ul>
-      <TodoItem v-for="todo in filterdTodos" 
-      :key="todo.id"
-      :todo = "todo" 
-      v-model:edited-todo="editedTodo"
-      @remove-todo="removeTodo"
+      <TodoItem
+        v-for="todo in filterdTodos"
+        :key="todo.id"
+        :todo="todo"
+        v-model:edited-todo="editedTodo"
+        @remove-todo="removeTodo"
       ></TodoItem>
     </ul>
 
     <!-- 过滤 -->
-    <Filter :items = "filterItems" v-model="visibility"></Filter>
+    <Filter :items="filterItems" v-model="visibility"></Filter>
   </div>
 </template>
 
 <script>
-import { reactive, toRefs, computed, watchEffect } from "vue";
+import { reactive, toRefs } from "vue";
 
-import TodoItem from './TodoItem.vue'
-import Filter from './Filter.vue'
-
-const filters = {
-  all(todos) {
-    return todos;
-  },
-  active(todos) {
-    return todos.filter((todo) => !todo.completed);
-  },
-  completed(todos) {
-    return todos.filter((todo) => todo.completed);
-  },
-};
-
-// 缓存操作
-const todoStorage = {
-  fetch() {
-    let todos = JSON.parse(localStorage.getItem("vue3-todos") || "[]");
-    todos.forEach((todo, index) => {
-      todo.id = index + 1;
-    });
-    return todos;
-  },
-  save(todos) {
-    localStorage.setItem("vue3-todos", JSON.stringify(todos));
-  },
-};
+import TodoItem from "./TodoItem.vue";
+import Filter from "./Filter.vue";
+import { useTodos } from "./useTodos";
+import { useFilter } from "./useFilter";
 
 export default {
   components: {
     TodoItem,
-    Filter
+    Filter,
   },
   setup() {
-    const state = reactive({
+    const todoState = reactive({
       newTodo: "",
-      todos: todoStorage.fetch(),
       editedTodo: null, //正在编辑的todo
-      filterItems:[
-        {
-          label:"All",
-          value:'all'
-        },
-        {
-          label:"Active",
-          value:'active'
-        },
-        {
-          label:"Completed",
-          value:'completed'
-        }
-      ],
-      visibility: "all",
-      filterdTodos: computed(() => {
-        return filters[state.visibility](state.todos);
-      }),
     });
+    const { todos, removeTodo, addTodo } = useTodos(todoState);
 
-    function addTodo() {
-      state.todos.push({
-        id: state.todos.length + 1,
-        title: state.newTodo,
-        completed: false,
-      });
-      state.newTodo = "";
-    }
-    
-    function removeTodo(todo) {
-      state.todos.splice(state.todos.indexOf(todo), 1);
-    }
-    
-    watchEffect(() => {
-      todoStorage.save(state.todos);
-    });
+    const filterState = useFilter(todos)
 
     return {
-      ...toRefs(state),
+      ...toRefs(todoState),
+      ...toRefs(filterState),
       addTodo,
-      removeTodo
+      removeTodo,
     };
-  }
+  },
 };
 </script>
 
 <style>
-
 </style>
