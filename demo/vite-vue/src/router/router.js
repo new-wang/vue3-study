1947175345ec6433991675cd85dd092f04c80f71
new-wang <1569566735@qq.com>
2022-01-06 11:04:51 +0800

    +：router 的变化一

--- demo/vite-vue/src/router/router.js
+++ demo/vite-vue/src/router/router.js
@@ -1,11 +1,15 @@
 import { h } from 'vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 
 import Dashboard from '../components/Dashboard.vue'
 import Todos from '../components/todos/Todos.vue'
+import Notfound from '../components/Notfound.vue'
 
+// 实例创建方式
 const router = createRouter({
-    history: createWebHashHistory(),
+    // history: createWebHashHistory(),
+    history: createWebHistory('/base-directory'),
+    // mode:'history',
     routes: [
         {
             path: '/',
@@ -16,9 +20,36 @@ const router = createRouter({
             path: '/todos',
             component: Todos,
             name:'todos'
+        },
+        {
+            path: '/:pathMatch(.*)*',
+            component: Notfound,
+            name:'not-found'
+        },
+        // 通配符 移除
+        // {
+        //     path: '/*',
+        // }
+    ],
+    scrollBehavior(to,from,savedPosition){
+        // {x:10,y:10}  now {left:10,top:10}
+        if (savedPosition) {
+            return savedPosition
+        } else {
+            return {top:10}
         }
-    ]
+    }
 })
+
+// 使用命名导航至404页面
+// router.resolve({
+//     name:'not-found',
+//     param:{
+//         pathMatch:['not','found']
+//     }
+// })
+// .href 
+// /not.found
 
 // 特性：动态路由
 
