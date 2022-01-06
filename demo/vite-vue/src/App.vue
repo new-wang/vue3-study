78b48f7b24a05ec0cf4f8c39d61b82a3255fd564
new-wang <1569566735@qq.com>
2022-01-06 14:22:09 +0800

    *：keep-alive 变化

--- demo/vite-vue/src/App.vue
+++ demo/vite-vue/src/App.vue
@@ -1,22 +1,28 @@
 <template>
-  <img alt="Vue logo" height="1000" src="./assets/logo.png" />
+  <!-- <img alt="Vue logo" height="1000" src="./assets/logo.png" /> -->
   <!-- <HelloWorld msg="Hello Vue 3.0 + Vite" /> -->
   <!-- <Todos></Todos> -->
 
-  <router-view></router-view>
+  <!-- <router-view></router-view> -->
 
+  <router-view v-slot="{ Component }">
+    <transition>
+      <keep-alive>
+        <component :is="Component" />
+      </keep-alive>
+    </transition>
+  </router-view>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 // import Todos from './components/todos/Todos.vue'
 
-
 export default {
-  name: 'App',
+  name: "App",
   components: {
     // HelloWorld,
     // Todos
-  }
-}
+  },
+};
 </script>
